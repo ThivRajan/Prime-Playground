4ea8a0ab92930ed527b6866ceaa04718f6d23ca5
Thivagar Nadarajan <thiv.nadarajan@gmail.com>
2021-03-04 19:56:30 -0500

    Move data into separate file

---
+++ data.js
@@ -0,0 +1,41 @@
+const btnNames = ['primary', 'secondary', 'success', 'warn', 'error'];
+
+const countries = ['New York', 'Rome', 'London', 'Paris', 'Istanbul'];
+
+const products = [
+	{
+		name: 'Headphones',
+		availability: 'in-stock',
+		category: 'Electronics',
+		price: '$299',
+		img: 'headphones',
+	},
+	{
+		name: 'Black Watch',
+		availability: 'low-stock',
+		category: 'Accessory',
+		price: '$133',
+		img: 'black-watch'
+	},
+	{
+		name: 'Galaxy Earrings',
+		availability: 'out-of-stock',
+		category: 'Accessory',
+		price: '$230',
+		img: 'galaxy-earrings'
+	},
+	{
+		name: 'Shoes',
+		availability: 'in-stock',
+		category: 'Apparel',
+		price: '$257',
+		img: 'shoes'
+	},
+	{
+		name: 'Purple Gemstone Necklace',
+		availability: 'low-stock',
+		category: 'Accessory',
+		price: '$158',
+		img: 'purple-gemstone-necklace'
+	}
+]
